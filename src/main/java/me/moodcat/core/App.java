0053527c2e6fd3796963971ea1cb8e5fab8e14d8
Tim van der Lippe <timlinks@hotmail.com>
2015-04-20 13:02:47 +0200

    Project configuration files for uniform code styling.

--- src/main/java/me/moodcat/core/App.java
+++ src/main/java/me/moodcat/core/App.java
@@ -1,10 +1,18 @@
 package me.moodcat.core;
 
 /**
+ * Test.
+ * <p>
  * Hello world!
+ * </p>
  */
 public class App {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+    
+    public static final void test() {
+        
     }
 }
