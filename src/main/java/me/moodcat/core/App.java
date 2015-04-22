3f2c2602edb9f260ad575b58057a6c83dd9f6424
Tim van der Lippe <timlinks@hotmail.com>
2015-04-22 11:50:04 +0200

    Updated pom.xml to include Jersey for the site backend.
    Added some annotations to App.java to test if it works (and presumably it does).

--- src/main/java/me/moodcat/core/App.java
+++ src/main/java/me/moodcat/core/App.java
@@ -1,18 +1,30 @@
 package me.moodcat.core;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Test.
  * <p>
  * Hello world!
  * </p>
  */
+@Path("/home")
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+
     public static final void test() {
-        
+
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String doSomething() {
+        return "Hello World!";
     }
 }
