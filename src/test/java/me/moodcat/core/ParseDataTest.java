8647b7966295b6c795b73484109f528523e26563
Jan-Willem Gmelig Meyling <j.gmeligmeyling@student.tudelft.nl>
2015-04-28 13:22:12 +0200

    Fixed the test

--- src/test/java/me/moodcat/core/ParseDataTest.java
+++ src/test/java/me/moodcat/core/ParseDataTest.java
@@ -3,17 +3,13 @@ package me.moodcat.core;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import junit.framework.Assert;
-import lombok.Data;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import lombok.Data;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 /**
  * @author Jan-Willem Gmelig Meyling
@@ -33,7 +29,7 @@ public class ParseDataTest {
 
         try( InputStream in = ParseDataTest.class.getResourceAsStream("/acousticbrainz/sample-data.json")) {
             BrainzResult result = objectMapper.readValue(in, BrainzResult.class);
-            Assert.assertEquals("C#", result.getTonal().getKeyKey());
+            assertEquals("C#", result.getTonal().getKeyKey());
         }
         catch (IOException e) {
             e.printStackTrace();
