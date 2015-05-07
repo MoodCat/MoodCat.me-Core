1d701df65b9f9153506a6ae833c2cc8093530ba6
Tim van der Lippe <timlinks@hotmail.com>
2015-05-07 10:43:56 +0200

    Formatted file properly.
    Make sure you have the intelliJ formatting plugin installed to make automated formatting possible.

--- src/test/java/me/moodcat/mood/classifier/MoodClassifierTest.java
+++ src/test/java/me/moodcat/mood/classifier/MoodClassifierTest.java
@@ -1,19 +1,21 @@
 package me.moodcat.mood.classifier;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import me.moodcat.database.embeddables.AcousticBrainzData;
-import me.moodcat.database.embeddables.VAVector;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
+import me.moodcat.database.embeddables.AcousticBrainzData;
+import me.moodcat.database.embeddables.VAVector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author Jan-Willem Gmelig Meyling
@@ -26,16 +28,20 @@ public class MoodClassifierTest {
 
     @Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-            { "/acousticbrainz/testData/100019264.json", new VAVector(-1, 1) }
+        return Arrays.asList(new Object[][] {
+                {
+                        "/acousticbrainz/testData/100019264.json", new VAVector(-1, 1)
+            }
         });
     }
 
     private final AcousticBrainzData data;
+
     private final VAVector expected;
+
     private final MoodClassifier moodClassifier;
 
-    public MoodClassifierTest(String path, VAVector expected) throws IOException {
+    public MoodClassifierTest(final String path, final VAVector expected) throws IOException {
         this.data = parseData(path);
         this.expected = expected;
         this.moodClassifier = new MoodClassifier();
@@ -44,17 +50,19 @@ public class MoodClassifierTest {
     @Test
     public void testMoodClassifierClassifiesExpectedValue() {
         final VAVector actual = this.moodClassifier.predict(this.data);
+
         assertVAVectorEquals(this.expected, actual);
     }
 
-    public static void assertVAVectorEquals(VAVector expected, VAVector actual) {
+    public static void assertVAVectorEquals(final VAVector expected, final VAVector actual) {
         assertEquals(expected.getArousal(), actual.getArousal(), EPSILON);
         assertEquals(expected.getValence(), actual.getValence(), EPSILON);
     }
 
-    public static AcousticBrainzData parseData(String path) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try(InputStream in = MoodClassifierTest.class.getResourceAsStream(path)) {
+    public static AcousticBrainzData parseData(final String path) throws IOException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        try (InputStream in = MoodClassifierTest.class.getResourceAsStream(path)) {
             return objectMapper.readValue(in, AcousticBrainzData.class);
         }
     }
