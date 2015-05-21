2a0029342216221cfc53123054dc87f1e93d8b26
Tim van der Lippe <timlinks@hotmail.com>
2015-05-21 09:57:10 +0200

    Revert removal of ArtistDAO.

---
+++ src/main/java/me/moodcat/database/controllers/ArtistDAO.java
@@ -0,0 +1,45 @@
+package me.moodcat.database.controllers;
+
+import static me.moodcat.database.entities.QArtist.artist;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import me.moodcat.database.entities.Artist;
+
+import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
+
+/**
+ * TODO: Add explanation.
+ *
+ * @author Jan-Willem Gmelig Meyling
+ */
+public class ArtistDAO extends AbstractDAO<Artist> {
+
+    @Inject
+    public ArtistDAO(final EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    @Transactional
+    public List<Artist> listArtists() {
+        return this.query().from(artist)
+                .list(artist);
+    }
+
+    @Transactional
+    public Artist findByName(final String name) {
+        return this.query().from(artist)
+                .where(artist.name.equalsIgnoreCase(name))
+                .singleResult(artist);
+    }
+
+    @Transactional
+    public Artist findById(final int id) {
+        return ensureExists(this.query().from(artist)
+                .where(artist.id.eq(id))
+                .singleResult(artist));
+    }
+}
