e2d54e7407e9e6b039437dbb991696fc6c3ce1c0
Tim van der Lippe <timlinks@hotmail.com>
2015-06-01 11:13:03 +0200

    Removed duplication factor in ExceptionMapperTests.
    Also added SoundCloudTrackTest.

--- src/test/java/me/moodcat/core/mappers/EntityNotFoundExceptionMapperTest.java
+++ src/test/java/me/moodcat/core/mappers/EntityNotFoundExceptionMapperTest.java
@@ -1,51 +1,19 @@
 package me.moodcat.core.mappers;
 
-import static org.junit.Assert.assertEquals;
-
 import javax.persistence.EntityNotFoundException;
-import javax.ws.rs.core.Response;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import javax.ws.rs.core.Response.Status;
 
 /**
  * @author Jaap Heijligers
  */
-public class EntityNotFoundExceptionMapperTest {
+public class EntityNotFoundExceptionMapperTest extends ExceptionMapperTest<EntityNotFoundException> {
 
-    private EntityNotFoundExceptionMapper mapper;
-
-    @Mock
-    private EntityNotFoundException exception;
-
-    @Before
-    public void before() {
-        mapper = new EntityNotFoundExceptionMapper();
-        exception = Mockito.mock(EntityNotFoundException.class);
+    public EntityNotFoundExceptionMapperTest() {
+        super(new EntityNotFoundExceptionMapper());
     }
 
-    @Test
-    public void testGetStatusCode() throws Exception {
-        assertEquals(mapper.getStatusCode(), Response.Status.NOT_FOUND);
-    }
-
-    @Test
-    public void testToResponse() throws Exception {
-        Response response = mapper.toResponse(exception);
-        AbstractExceptionMapper.ExceptionResponse responseEntity = (AbstractExceptionMapper.ExceptionResponse) response
-                .getEntity();
-        assertEquals(responseEntity.getMessage(), exception.getMessage());
-    }
-
-    @Test
-    public void testToResponseMessage() throws Exception {
-        final String message = "Test Message";
-        Mockito.when(exception.getMessage()).thenReturn(message);
-        Response response = mapper.toResponse(exception);
-        AbstractExceptionMapper.ExceptionResponse responseEntity = (AbstractExceptionMapper.ExceptionResponse) response
-                .getEntity();
-        assertEquals(responseEntity.getMessage(), message);
+    @Override
+    protected Status getResponseStatus() {
+        return Status.NOT_FOUND;
     }
 }
