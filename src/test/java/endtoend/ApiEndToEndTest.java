29c1d901eb2e50c485aa617585be536c3fe468ad
Tim van der Lippe <timlinks@hotmail.com>
2015-06-19 13:51:30 +0200

    Test that might be failing. Let's see.

--- src/test/java/endtoend/ApiEndToEndTest.java
+++ src/test/java/endtoend/ApiEndToEndTest.java
@@ -1,6 +1,11 @@
 package endtoend;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.ws.rs.core.GenericType;
+
 import me.moodcat.api.models.RoomModel;
 import me.moodcat.api.models.SongModel;
 
@@ -9,7 +14,7 @@ import org.junit.Test;
 public class ApiEndToEndTest extends EndToEndTest {
 
     @Test
-    public void canRetrieveRooms() {
+    public void canRetrieveRoom() {
         RoomModel room = this.performGETRequest(RoomModel.class, "rooms/1");
         
         assertEquals(1, room.getId().intValue());
@@ -22,6 +27,13 @@ public class ApiEndToEndTest extends EndToEndTest {
         
         assertEquals(1, song.getId().intValue());
         assertEquals(202330997, song.getSoundCloudId().intValue());
+    }
+    
+    @Test
+    public void canRetrieveRooms() {
+        List<RoomModel> rooms = this.performGETRequestWithGenericType(new GenericType<List<RoomModel>>(){}, "rooms");
+        
+        assertEquals(1, rooms.get(0).getId().intValue());
     }
 
 }
